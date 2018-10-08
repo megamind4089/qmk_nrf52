9c638c8211a39984e9215a1ed53fd5f8b2e88417
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2018-10-08 15:49:26 +0900

    Keyboard: Add helix_ble

---
+++ keyboards/helix_ble/helix_ble.c
@@ -0,0 +1,1 @@
+#include "helix_ble.h"
---
+++ keyboards/helix_ble/slave/config.h
@@ -0,0 +1,19 @@
+#ifndef SLAVE_CONFIG_H_
+#define SLAVE_CONFIG_H_
+
+#include "custom_board.h"
+
+#if HELIX_ROWS == 4
+  #define THIS_DEVICE_ROWS HELIX_ROWS
+  #define MATRIX_ROW_PINS { PIN7, PIN8, PIN9, PIN10 }
+#elif HELIX_ROWS == 5
+  #define THIS_DEVICE_ROWS HELIX_ROWS
+  #define MATRIX_ROW_PINS { PIN7, PIN8, PIN9, PIN10, PIN11 }
+#endif
+
+#define THIS_DEVICE_COLS 7
+#define MATRIX_COL_PINS { PIN20, PIN19, PIN18, PIN17, PIN16, PIN15, PIN14 }
+
+#define IS_LEFT_HAND  false
+
+#endif /* SLAVE_CONFIG_H_ */
