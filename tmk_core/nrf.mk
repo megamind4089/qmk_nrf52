10e6b3c5873f8d0c09b7e2af2e74207149e601e1
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2018-10-09 08:02:59 +0900

    Call nrfutil from makefile to package and program

--- tmk_core/nrf.mk
+++ tmk_core/nrf.mk
@@ -825,6 +825,36 @@ dfu-util: $(BUILD_DIR)/$(TARGET).bin cpfirmware sizeafter
 
 bin: $(BUILD_DIR)/$(TARGET).bin sizeafter
 	$(COPY) $(BUILD_DIR)/$(TARGET).bin $(TARGET).bin;
+	
+GREP ?= grep
+NRFUTIL ?= nrfutil
+nrfutil:
+	if ! type "nrfutil" > /dev/null 2>&1; then \
+		echo 'ERROR: nrfutil is not found'; exit 1;\
+	fi	
+	$(NRFUTIL) pkg generate --debug-mode --hw-version 52 --sd-req 0xA9 --application $(TARGET).bin $(TARGET).zip
+	if $(GREP) -q -s Microsoft /proc/version; then \
+		echo 'ERROR: nrfutil cannot be automated within the Windows Subsystem for Linux (WSL) currently.'; \
+	else \
+		printf "Detecting USB port, put your controller into dfu-mode now."; \
+		ls /dev/tty* > /tmp/1; \
+		while [ -z $$USB ]; do \
+			sleep 0.5; \
+			printf "."; \
+			ls /dev/tty* > /tmp/2; \
+			USB=`comm -13 /tmp/1 /tmp/2 | $(GREP) -o '/dev/tty.*'`; \
+			mv /tmp/2 /tmp/1; \
+		done; \
+		echo ""; \
+		echo "Detected controller on USB port at $$USB"; \
+		if $(GREP) -q -s 'MINGW\|MSYS' /proc/version; then \
+			USB=`echo "$$USB" | perl -pne 's/\/dev\/ttyS(\d+)/COM.($$1+1)/e'`; \
+			echo "Remapped MSYS2 USB port to $$USB"; \
+		fi; \
+		sleep 1; \
+		echo "Programming Started"; \
+		$(NRFUTIL) dfu usb_serial -pkg $(TARGET).zip -p $$USB; \
+	fi
 
 elf: $(NRFLIB)
 
