ef9e02f27fd47ee60ad0cdf3d6dd8537cba4595b
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2018-12-31 15:41:39 +0900

    Fix bug: Use environmantal path for Ergodash BLE

--- keyboards/ergodash_ble/master/rules.mk
+++ keyboards/ergodash_ble/master/rules.mk
@@ -2,7 +2,7 @@
 # it should exist either in <this_dir>/ld/
 MCU_LDSCRIPT = nrf52840
 MCU_SERIES = NRF52840
-NRFSDK_ROOT := /c/dev/nRF5_SDK_15.0.0_a53641a # Path to nRF SDK v12.3.0
+NRFSDK_ROOT := $(NRFSDK15_ROOT) #Path to nRF SDK v15.0.0
 
 # project specific files
 #SRC =	matrix.c
