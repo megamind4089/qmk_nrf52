2ad8c7492882f86d5f42e0591fef348e49a89771
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2019-01-09 21:18:37 +0900

    Fix bug: Add declaration of read_row_ioexpander()

--- tmk_core/protocol/nrf/io_expander.h
+++ tmk_core/protocol/nrf/io_expander.h
@@ -8,9 +8,15 @@
 #ifndef TMK_CORE_PROTOCOL_NRF_IO_EXPANDER_H_
 #define TMK_CORE_PROTOCOL_NRF_IO_EXPANDER_H_
 
+#ifdef USE_I2C_IOEXPANDER
+
+#include "matrix.h"
+
 typedef enum {
   TCA9555
 } IOEXPANDER_TYPEDEF;
 
+matrix_row_t read_row_ioexpander (uint8_t row);
 
+#endif
 #endif /* TMK_CORE_PROTOCOL_NRF_IO_EXPANDER_H_ */
