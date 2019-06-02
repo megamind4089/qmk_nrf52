b3f8a7f7277d560d5438df0f96c0f49d3ec54c52
HATANO Hiromichi <hatano@sarasarado.org>
2019-06-02 23:10:14 +0900

    add copyright

--- keyboards/cscs64_ble/config.h
+++ keyboards/cscs64_ble/config.h
@@ -2,6 +2,7 @@
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
 Copyright 2018 Sekigon
+Copyright 2019 hatano.h
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- keyboards/cscs64_ble/master/master.c
+++ keyboards/cscs64_ble/master/master.c
@@ -1,5 +1,6 @@
 /*
 Copyright 2018 Sekigon
+Copyright 2019 hatano.h
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
