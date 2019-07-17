5b87534f63eb2abd0974aa56969b2b03cee0ad0a
Minori Tokuda <minorityland@gmail.com>
2019-07-17 15:47:40 +0900

    Fix for Nomu30 BLE support (#1)
    * add valid pin assigns and BLE related keys to nomu30_ble
    
    * fix nrfutil subcommand name.
    
    Because the version 7.0 of "click" library in "nrfutil" replaces dashes to
    underscores in subcommand name.

--- keyboards/nomu30_ble/keymaps/default/keymap.c
+++ keyboards/nomu30_ble/keymaps/default/keymap.c
@@ -42,12 +42,13 @@ enum custom_keycodes {
 #define _NUMS 1
 #define _SYMS 2
 #define _FUNC 3
+#define _BLE  4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT,
-        KC_LSFT, LALT_T(KC_Z), LGUI_T(KC_X), KC_C, LT(_SYMS, KC_V), LT(_FUNC, KC_B), LT(_NUMS, KC_N), KC_M, KC_SPC
+        KC_LSFT, LALT_T(KC_Z), LGUI_T(KC_X), LT(_BLE, KC_C), LT(_SYMS, KC_V), LT(_FUNC, KC_B), LT(_NUMS, KC_N), KC_M, KC_SPC
     ),
     [_NUMS] = LAYOUT(
         KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV,
@@ -63,6 +64,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
         KC_TAB, KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, RESET,
         KC_TRNS, KC_LALT, KC_LGUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    [_BLE] = LAYOUT(
+        ADV_ID0, ADV_ID1, ADV_ID2, ADV_ID3, ADV_ID4, DEL_ID0, DEL_ID1, DEL_ID2, DEL_ID3, DEL_ID4, DELBNDS,
+        BATT_LV, USB_EN,  USB_DIS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BLE_EN,  BLE_DIS, KC_TRNS, AD_WO_L,
+        ENT_SLP, KC_LALT, KC_LGUI, ENT_DFU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 
